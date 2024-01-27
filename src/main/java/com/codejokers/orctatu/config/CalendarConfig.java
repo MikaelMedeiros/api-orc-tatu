cda0e8e4151ea632628321f67e693e616647eb66
Mikael Medeiros <mikaeltruth@gmail.com>
2024-01-27 16:52:19 -0300

    configuracao e chamada para adicionar evento no google calendar

---
+++ src/main/java/com/codejokers/orctatu/config/CalendarConfig.java
@@ -0,0 +1,25 @@
+package com.codejokers.orctatu.config;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.calendar.Calendar;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class CalendarConfig {
+
+    public Calendar serviceCalendar(String token) {
+        //identificar uma forma de utilizar o credential não depreciado
+        GoogleCredential credential = new GoogleCredential().setAccessToken(token);
+
+        return new Calendar.Builder(
+            new NetHttpTransport(),
+            new GsonFactory(),
+                credential
+
+        ).build();
+    }
+}
---
+++ src/main/java/com/codejokers/orctatu/controller/EventController.java
@@ -0,0 +1,85 @@
+package com.codejokers.orctatu.controller;
+
+import com.codejokers.orctatu.config.CalendarConfig;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "events", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class EventController {
+
+    private final CalendarConfig calendarConfig;
+
+    @PostMapping
+    public ResponseEntity<String> agendarTatuagem(
+            @RequestBody Map<String, String> json,
+            @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal
+    ) throws IOException {
+
+        //Colocar o titulo, descricao e local
+        Event event = new Event()
+                .setSummary(json.get("titulo"))
+                .setLocation("800 Howard St., San Francisco, CA 94103")
+                .setDescription(json.get("descricao"));
+
+
+        //reservar tempo na agenda para a tatuagem
+        DateTime startDateTime = new DateTime("2024-01-28T09:00:00-07:00");
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDateTime)
+                .setTimeZone("America/Sao_Paulo");
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2024-01-28T09:00:00-10:00");
+        EventDateTime end = new EventDateTime()
+                .setDateTime(endDateTime)
+                .setTimeZone("America/Los_Angeles");
+        event.setEnd(end);
+
+        //adicioanr participantes
+        EventAttendee[] attendees = new EventAttendee[] {
+                new EventAttendee().setEmail("juliamellopacheco@gmail.com"),
+                new EventAttendee().setEmail("mikaeltruth@gmail.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+        //Quantos minutos antes sera lembrado da tatto
+        EventReminder[] reminderOverrides = new EventReminder[] {
+                new EventReminder().setMethod("email").setMinutes(24 * 60),
+                new EventReminder().setMethod("popup").setMinutes(10),
+        };
+
+        Event.Reminders reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(Arrays.asList(reminderOverrides));
+        event.setReminders(reminders);
+
+        //seleciona calendario principal do tatuador
+        String calendarId = "primary";
+
+        //Cria conexão com a api do google calendar
+        var service = calendarConfig.serviceCalendar(principal.getAttribute("token").toString());
+
+        event = service.events().insert(calendarId, event).execute();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Tatuagem agendada!: " +  event.getHtmlLink());
+    }
+}
