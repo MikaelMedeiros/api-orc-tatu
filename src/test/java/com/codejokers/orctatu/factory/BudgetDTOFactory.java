2124ad1b6f6252ea429cebb9d7b8a83a3b452f77
Mikael Medeiros <mikaeltruth@gmail.com>
2024-02-16 00:17:12 -0300

    correcao teste unitario bodylocals

--- src/test/java/com/codejokers/orctatu/factory/BudgetDTOFactory.java
+++ src/test/java/com/codejokers/orctatu/factory/BudgetDTOFactory.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BudgetDTOFactory {
 
-    public static final String BUDGET_DTO_JSON = "{\"clientName\":\"John Doe\",\"draw\":\"Borboleta\",\"centimeter\":40,\"pricePerCentimeter\":20,\"bodyLocals\":[\"LEG\",\"ARM\"],\"styles\":[\"FINELINE\",\"REALISM\"],\"details\":[\"SHADING\",\"LINES\"],\"description\":\"xxxxx\",\"studioPercentage\":20,\"parkingCost\":10,\"materialCost\":190,\"creditCardFee\":20}";
+    public static final String BUDGET_DTO_JSON = "{\"clientName\":\"John Doe\",\"draw\":\"Borboleta\",\"centimeter\":40,\"pricePerCentimeter\":20,\"bodyLocal\":\"LEG\",\"styles\":[\"FINELINE\",\"REALISM\"],\"details\":[\"SHADING\",\"LINES\"],\"description\":\"xxxxx\",\"studioPercentage\":20,\"parkingCost\":10,\"materialCost\":190,\"creditCardFee\":20}";
 
     public static BudgetDTO createBudgetDTO() throws JsonProcessingException {
         return new ObjectMapper().readValue(BUDGET_DTO_JSON, BudgetDTO.class);
--- src/test/java/com/codejokers/orctatu/factory/BudgetFactory.java
+++ src/test/java/com/codejokers/orctatu/factory/BudgetFactory.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BudgetFactory {
 
-    private static final String BUDGET_JSON = "{\"id\":1,\"googleId\":\"12345\",\"clientName\":\"John Doe\",\"draw\":\"Borboleta\",\"centimeter\":40,\"pricePerCentimeter\":20,\"bodyLocals\":[\"LEG\",\"ARM\"],\"styles\":[\"FINELINE\",\"REALISM\"],\"details\":[\"SHADING\",\"LINES\"],\"description\":\"xxxxx\",\"studioPercentage\":20,\"parkingCost\":10,\"materialCost\":190,\"creditCardFee\":20,\"tattooValue\":800,\"studioFee\":160.0,\"netValue\":640.0,\"grossValue\":1000,\"creditCardValue\":1020,\"status\":\"BUDGETED\",\"paymentMethod\":null}";
+    private static final String BUDGET_JSON = "{\"id\":1,\"googleId\":\"12345\",\"clientName\":\"John Doe\",\"draw\":\"Borboleta\",\"centimeter\":40,\"pricePerCentimeter\":20,\"bodyLocal\":\"LEG\",\"styles\":[\"FINELINE\",\"REALISM\"],\"details\":[\"SHADING\",\"LINES\"],\"description\":\"xxxxx\",\"studioPercentage\":20,\"parkingCost\":10,\"materialCost\":190,\"creditCardFee\":20,\"tattooValue\":800,\"studioFee\":160.0,\"netValue\":640.0,\"grossValue\":1000,\"creditCardValue\":1020,\"status\":\"BUDGETED\",\"paymentMethod\":null}";
 
     public static Budget createBudget() throws JsonProcessingException {
         return new ObjectMapper().readValue(BUDGET_JSON, Budget.class);
